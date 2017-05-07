0a49c73caa44ec4785e1d6bd5cd7db92da8d6fe6
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 18:22:38 -0400

    is compare working?

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (request, response) {
     console.log(request.method);
                                
     if (request.method == 'POST') {
-        
+        console.log("boolean worked");
         /*
         request.on('data', function (data) {
                    body += data;
