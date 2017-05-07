19dd22cf5eee404ba535d485ed0f4b21ff1176fe
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 19:07:06 -0400

    only output end result

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var server = http.createServer(function (request, response) {
     if (request.method == 'POST') {
         request.on('data', function (data) {
                    body += data.toString();
-                   console.log("\nreceiving data\n");
-                   console.log(data);
+                   //console.log("\nreceiving data\n");
+                   //console.log(data);
                    // Too much POST data, kill the connection!
                    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
                    /*if (body.length > 1e9)
