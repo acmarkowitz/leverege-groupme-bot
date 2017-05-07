603508d9eab5ba63093825dc5c994cccab66d6a8
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 18:04:15 -0400

    proper port

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ var server = http.createServer(function (request, response) {
     console.log(post.text);
     response.statusCode = 404;
     response.end();
-}).listen(8080);
+}).listen(process.env.PORT || 5000);
 
 console.log("\nHello World");
