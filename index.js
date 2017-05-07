1720d318344512310cd00977377a32d6817625f1
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 14:19:11 -0400

    Debug

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
     if (request.method == 'POST') {
+        console.log("this far\n");
         var body = '';
         
         request.on('data', function (data) {
