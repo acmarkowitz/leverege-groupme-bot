de51e1c3e0eaa24eeacc253a4803ec44d72be8ec
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 18:52:25 -0400

    more ram

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var server = http.createServer(function (request, response) {
                    console.log(data);
                    // Too much POST data, kill the connection!
                    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                   if (body.length > 1e6)
+                   if (body.length > 1e9)
                    request.connection.destroy();
                    });
         
