519be9ee97667ca3b9c01ec4f7e47e7f380e2bb7
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-08 08:32:22 -0400

    collapsing messages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var HTTPS = require('https');
 
 // To send text into the Groupme chat
-var options = {
+var groupMeOptions = {
     host: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST',
@@ -17,8 +17,7 @@ var movieOptions = {
     "headers": {}
 };
 // Bot_id and initial text for the groupme message
-var groupmeRequest =
-{
+var groupmeRequest = {
     bot_id: "2ae846f9593ef32b98600483ea",
     text: "Upcoming releases:\n"
 };
@@ -40,20 +39,12 @@ function wantMovies (data) {
         var movieReq = HTTPS.request(movieOptions, handleMDB);
         movieReq.write("{}"); // Complete the request
         movieReq.end();
-        var gmReq = HTTPS.request(options);
-        var toWrite = JSON.stringify(groupmeRequest);
-        console.log("Sending this to groupme: " + toWrite);
-        gmReq.write(toWrite); // Write the POST to the groupme chat
-        gmReq.end();
     }
 }
 function handleMDB(movieRes) {
-    movieRes.on("data", prepareMessage);
-    //movieRes.on("end", prepareMessage);
+    movieRes.on("data", prepareMessage,sendMovies);
 }
 function prepareMessage(chunk) {
-    //chunks.push(chunk);
-    //var movieBody = Buffer.concat(chunk);
     var res = JSON.parse(chunk.toString());   // Format as JSON
     var numResults = res.results.length;          // Get number of movies
     for (var i = 0; i < numResults; i++) {        // Iterate over movies for title, date
@@ -63,15 +54,9 @@ function prepareMessage(chunk) {
         + "\n";
     }
 }
-/*
-function prepareMessage() {
-    var movieBody = Buffer.concat(chunks);
-    var res = JSON.parse(movieBody.toString());   // Format as JSON
-    var numResults = res.results.length;          // Get number of movies
-    for (var i = 0; i < numResults; i++) {        // Iterate over movies for title, date
-        groupmeRequest.text += res.results[i].release_date
-        + ": "
-        + res.results[i].title
-        + "\n";
-    }
-} */
+function sendMovies () {
+    var gmReq = HTTPS.request(groupMeOptions);
+    var toWrite = JSON.stringify(groupmeRequest);
+    gmReq.write(toWrite); // Write the POST to the groupme chat
+    gmReq.end();
+}
