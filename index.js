6c73c76e50fb821ff60586bee41ecd759c4b949d
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-07 20:50:34 -0400

    send to groupme

--- index.js
+++ index.js
@@ -9,20 +9,33 @@ var server = http.createServer(function (request, response) {
     var body;
                                
     if (request.method == 'POST') {
-        request.on('data', function (data) {
-                   body = JSON.parse(data);
-                   }
-                   );
-        request.on('end', function () {
-                   var text = body.text.toLocaleLowerCase();
-                   if (text.search("movie") != -1) {
-                   console.log("Yes");
-                   }
-                   else {
-                   console.log("No");
-                   }
-                   }
-                   );
+       request.on('data', function (data) {
+          body = JSON.parse(data);
+       });
+       request.on('end', function () {
+          var text = body.text.toLocaleLowerCase();
+          if (text.search("movie") != -1) {
+              console.log("Yes");
+              // Configure the request
+              var options = {
+              url: 'https://api.groupme.com/v3/bots/post',
+              method: 'POST',
+              headers: 'Content-Type: application/json',
+              form: {'bot_id': '2ae846f9593ef32b98600483ea',
+                  'text': 'Yes, movie'}
+              // Start the request
+              request(options, function (error, resp, bd) {
+                    if (!error && resp.statusCode == 200) {
+                    // Print out the response body
+                    console.log(bd)
+                    }
+              })
+          }
+          }
+          else {
+             console.log("No");
+          }
+       });
     }
     response.statusCode = 400;
     response.end();
