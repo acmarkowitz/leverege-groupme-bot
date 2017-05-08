1989d297915392a922a7782ca9d889ad44444b6b
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-08 01:51:05 -0400

    Comments

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var http = require('http');
 var HTTPS = require('https');
 
+// To send text into the Groupme chat
 var options = {
     host: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST',
     headers: {'Content-Type': 'application/json'}
 };
+// To retrieve information from the movie DB
 var movieOptions = {
     "method": "GET",
     "hostname": "api.themoviedb.org",
@@ -14,12 +16,13 @@ var movieOptions = {
     "path": "/3/movie/upcoming?region=US&page=1&language=en-US&api_key=059eb0be9524d13c1469b5ed1e1155f1",
     "headers": {}
 };
+// Bot_id and initial text for the groupme message
 var groupmeRequest =
 {
     bot_id: "2ae846f9593ef32b98600483ea",
     text: "Upcoming releases:\n"
 };
-
+// Get the upcoming releases in the US when it is time; code from example on moviedb.org
 function getMovies() {
     var movieReq = HTTPS.request(movieOptions, function (movieRes) {
        var chunks = [];
@@ -28,9 +31,9 @@ function getMovies() {
        });
        movieRes.on("end", function () {
           var movieBody = Buffer.concat(chunks);
-          var res = JSON.parse(movieBody.toString());
-          var numResults = res.results.length;
-          for (var i = 0; i < numResults; i++) {
+          var res = JSON.parse(movieBody.toString());   // Format as JSON
+          var numResults = res.results.length;          // Get number of movies
+          for (var i = 0; i < numResults; i++) {        // Iterate over movies for title, date
              groupmeRequest.text += res.results[i].release_date
                                  + ": "
                                  + res.results[i].title
@@ -38,31 +41,27 @@ function getMovies() {
           }
        });
     });
-    movieReq.write("{}");
+    movieReq.write("{}"); // Complete the request
     movieReq.end();
 }
+// Run server to listen for groupme messages
 var server = http.createServer(function (servReq, servRep) {
-    //console.log("Request method is: " + servReq.method);
     var body = '';
-                               
+    // Has there been a post?
     if (servReq.method == 'POST') {
-       servReq.on('data', function (data) {
+       servReq.on('data', function (data) { // Add the data to the list
           body += data;
-                  //console.log(data);
        });
        servReq.on('end', function () {
+          // make all message text lower case for easy comparison
           var text = JSON.parse(body).text.toLocaleLowerCase();
-                  //console.log(text);
+          // If "movie" is in the text...
           if (text.search("movie") != -1) {
-              //console.log("Yes");
-              getMovies();
+              getMovies(); // get the upcoming releases, add them to GroupMe text
               var gmReq = HTTPS.request(options);
               var toWrite = JSON.stringify(groupmeRequest);
-              gmReq.write(toWrite);
+              gmReq.write(toWrite); // Write the POST to the groupme chat
               gmReq.end();
-          }
-          else {
-             //console.log("No");
           }
        });
     }
