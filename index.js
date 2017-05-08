4ce7970a7397c26a65264fee10c075bbaad0c030
Anthony Markowitz <acmarkowitz@gmail.com>
2017-05-08 06:52:10 -0400

    back to callback hell

--- index.js
+++ index.js
@@ -22,56 +22,50 @@ var groupmeRequest =
     bot_id: "2ae846f9593ef32b98600483ea",
     text: "Upcoming releases:\n"
 };
+// Get the upcoming releases in the US when it is time; code from example on moviedb.org
+function getMovies() {
+    var movieReq = HTTPS.request(movieOptions, function (movieRes) {
+       var chunks = [];
+       movieRes.on("data", function (chunk) {
+          chunks.push(chunk);
+       });
+       movieRes.on("end", function () {
+          var movieBody = Buffer.concat(chunks);
+          var res = JSON.parse(movieBody.toString());   // Format as JSON
+          var numResults = res.results.length;          // Get number of movies
+          for (var i = 0; i < numResults; i++) {        // Iterate over movies for title, date
+             groupmeRequest.text += res.results[i].release_date
+                                 + ": "
+                                 + res.results[i].title
+                                 + "\n";
+          }
+       });
+    });
+    movieReq.write("{}"); // Complete the request
+    movieReq.end();
+}
 // Run server to listen for groupme messages
 var server = http.createServer(function (servReq, servRep) {
     var body = '';
     // Has there been a post?
     if (servReq.method == 'POST') {
-       servReq.on('data', addData(body));
-       servReq.on('end', movieMentioned(body) ,servRep.end());
+       servReq.on('data', function (data) { // Add the data to the list
+          body += data;
+       });
+       servReq.on('end', function () {
+          // make all message text lower case for easy comparison
+          var text = JSON.parse(body).text.toLocaleLowerCase();
+          // If "movie" is in the text...
+          if (text.search("movie") != -1) {
+              getMovies(); // get the upcoming releases, add them to GroupMe text
+              var gmReq = HTTPS.request(options);
+              var toWrite = JSON.stringify(groupmeRequest);
+              gmReq.write(toWrite); // Write the POST to the groupme chat
+              gmReq.end();
+          }
+       },servRep.end());
     }
     else {
     servRep.end();
     }
 }).listen(process.env.PORT || 5000);
-
-// Get the upcoming releases in the US when it is time; code from example on moviedb.org
-function getMovies() {
-    var movieReq = HTTPS.request(movieOptions, askForMovies());
-    movieReq.write("{}"); // Complete the request
-    movieReq.end();
-}
-function pushChunks(chunk, chunks) {
-    chunks.push(chunk);
-}
-function findTitles () {
-    var movieBody = Buffer.concat(chunks);
-    var res = JSON.parse(movieBody.toString());   // Format as JSON
-    var numResults = res.results.length;          // Get number of movies
-    for (var i = 0; i < numResults; i++) {        // Iterate over movies for title, date
-        groupmeRequest.text += res.results[i].release_date
-        + ": "
-        + res.results[i].title
-        + "\n";
-    }
-}
-function askForMovies(movieRes) {
-    var chunks = [];
-    movieRes.on("data", pushChunks(chunks));
-    movieRes.on("end", findTitles());
-}
-function addData (data,body) { // Add the data to the list
-    body += data;
-}
-function movieMentioned (body) {
-    // make all message text lower case for easy comparison
-    var text = JSON.parse(body).text.toLocaleLowerCase();
-    // If "movie" is in the text...
-    if (text.search("movie") != -1) {
-        getMovies(); // get the upcoming releases, add them to GroupMe text
-        var gmReq = HTTPS.request(options);
-        var toWrite = JSON.stringify(groupmeRequest);
-        gmReq.write(toWrite); // Write the POST to the groupme chat
-        gmReq.end();
-    }
-}
